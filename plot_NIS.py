5f716bb466cea1be9dc6e622f9dd82cfd5a7d2e1
hankkkwu <two30022001@hotmail.com>
2019-06-20 00:28:55 +0800

    Update plot_NIS.py

--- plot_NIS.py
+++ plot_NIS.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-nis = []
-f=open("build/output.txt", "r")
-f1 = f.readlines()
-for i in f1:
-    nis.append(float(i[:-2]))
-# t = np.arange(0, len(nis), 1)
-plt.plot(nis)
+lidar_nis = []
+baseline_l = []
+radar_nis = []
+baseline_r = []
+f_lidar = open("build/lidar_output.txt", "r")
+f_radar = open("build/radar_output.txt", "r")
+f1 = f_lidar.readlines()
+f2 = f_radar.readlines()
+for l in f1:
+    lidar_nis.append(float(l[:-2]))
+    baseline_l.append(5.991)
+for r in f2:
+    radar_nis.append(float(r[:-2]))
+    baseline_r.append(7.815)
+
+plt.plot(radar_nis, label="radar_nis")
+plt.plot(baseline_r, label="95%")
+# plt.plot(lidar_nis, label="radar_nis")
+# plt.plot(baseline_l, label="95%")
+plt.legend()   # show lines name
+plt.xlim(0, 250)
+plt.ylim(0, 16)
 plt.ylabel("NIS")
 plt.show()
